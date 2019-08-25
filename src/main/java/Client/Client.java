9072d61bbba3b4e3a035a0097faaa014f944f86c
Ankitagole <ankita17@navgurukul.org>
2019-08-25 15:33:32 +0530

    print multiple rectangle areas

---
+++ src/main/java/Client/Client.java
@@ -0,0 +1,22 @@
+package Client;
+
+import shapeMensuration.Rectangle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client {
+    public static void main(String[] args) {
+        List<Double> rectanglesArea = new ArrayList<>();
+        Rectangle rectangle1 = new Rectangle(2, 3);
+        Rectangle rectangle2 = new Rectangle(3, 3);
+        Rectangle rectangle3 = new Rectangle(4, 3);
+        Rectangle rectangle4 = new Rectangle(3, 5);
+        rectanglesArea.add(rectangle1.getArea());
+        rectanglesArea.add(rectangle2.getArea());
+        rectanglesArea.add(rectangle3.getArea());
+        rectanglesArea.add(rectangle4.getArea());
+
+        System.out.println(rectanglesArea);
+        }
+    }
