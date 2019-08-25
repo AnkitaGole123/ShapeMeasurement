450ed1db435464bfd7ed018cabeb01dc369a5f7b
Ankitagole <ankita17@navgurukul.org>
2019-08-25 14:53:08 +0530

    add. testCase of sphere volume

--- src/test/java/shapeMensurationTest/SphereTest.java
+++ src/test/java/shapeMensurationTest/SphereTest.java
@@ -1,5 +1,17 @@
 package shapeMensurationTest;
 
+import org.junit.Test;
+import shapeMensuration.Sphere;
+
+import static org.junit.Assert.assertEquals;
+
 public class SphereTest {
+    @Test
+    public void itShouldGiveTheVolumeOfSphere() {
+        Sphere sphere = new Sphere(3);
+        double expected = 113;
+        double delta = 1;
+        assertEquals(expected,sphere.getVolume(),delta);
+    }
 
 }
