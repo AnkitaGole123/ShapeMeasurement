23881951688ab4e96f2af1ecae7a09dd1cef9993
Ankitagole <ankita17@navgurukul.org>
2019-08-26 09:16:37 +0530

    refactor. adding element inside loop

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -7,18 +7,10 @@ import java.util.List;
 
 public class Client {
     public static void main(String[] args) {
-        List<Double> rectanglesArea = new ArrayList<>();
-        Rectangle rectangle1 = new Rectangle(2, 3);
-        Rectangle rectangle2 = new Rectangle(3, 3);
-        Rectangle rectangle3 = new Rectangle(4, 3);
-        Rectangle rectangle4 = new Rectangle(3, 5);
-        rectanglesArea.add(rectangle1.getArea());
-        rectanglesArea.add(rectangle2.getArea());
-        rectanglesArea.add(rectangle3.getArea());
-        rectanglesArea.add(rectangle4.getArea());
-        for (int i = 0; i < rectanglesArea.size(); i++) {
-            System.out.println(rectanglesArea.get(i));
-        }
-
+        List<Rectangle> rectangles = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            rectangles.add(new Rectangle(1,1));
+            System.out.println(rectangles.get(i).getPerimeter());
         }
     }
+}
