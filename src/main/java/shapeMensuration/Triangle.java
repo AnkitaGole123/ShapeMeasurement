84492e4b04e63ad5224203f05a9cd36c77188d6b
Ankitagole <ankita17@navgurukul.org>
2019-08-27 13:10:54 +0530

    create a field and constructor

--- src/main/java/shapeMensuration/Triangle.java
+++ src/main/java/shapeMensuration/Triangle.java
@@ -2,6 +2,14 @@ package shapeMensuration;
 
 public class Triangle implements Shape{
 
+    public Triangle(double hieght, double width) {
+        this.height = hieght;
+        this.width = width;
+    }
+
+    double height;
+    double width;
+
     @Override
     public double getArea() {
         return 0;
