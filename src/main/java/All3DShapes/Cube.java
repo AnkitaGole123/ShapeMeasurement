814ae7ebe3495dec822dad72cadc05faf4825511
Ankitagole <ankita17@navgurukul.org>
2019-08-27 22:36:34 +0530

    create constructor

--- src/main/java/All3DShapes/Cube.java
+++ src/main/java/All3DShapes/Cube.java
@@ -1,4 +1,19 @@
 package All3DShapes;
 
-public class Cube {
+public class Cube implements Shapes3D{
+    public Cube(double length) {
+        this.length = length;
+    }
+
+    double length;
+
+    @Override
+    public double getVolume() {
+        return 0;
+    }
+
+    @Override
+    public double getSurfaceArea() {
+        return 0;
+    }
 }
