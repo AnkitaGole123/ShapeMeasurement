72974da69ea94f5db1d1d3d6881e59761130fd16
Ankitagole <ankita17@navgurukul.org>
2019-08-27 14:09:53 +0530

    add. testCase for getArea of triangle

---
+++ src/test/java/shapeMensurationTest/TriangleTest.java
@@ -0,0 +1,17 @@
+package shapeMensurationTest;
+
+import org.junit.Test;
+import shapeMensuration.Triangle;
+
+import static org.junit.Assert.assertEquals;
+
+public class TriangleTest {
+    @Test
+    public void itShouldGiveTheAreaOfRectangle() {
+        Triangle triangle = new Triangle(3,6);
+        double expected = 9;
+        double delta = 1;
+        assertEquals(expected,triangle.getArea(),delta);
+
+    }
+}
