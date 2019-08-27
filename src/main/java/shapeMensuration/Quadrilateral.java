86096f7180971a30ef94f5cc8e7b1ecc2bc8f53e
Ankitagole <ankita17@navgurukul.org>
2019-08-27 16:53:10 +0530

    Add. create field of quadrilateral

--- src/main/java/shapeMensuration/Quadrilateral.java
+++ src/main/java/shapeMensuration/Quadrilateral.java
@@ -1,4 +1,12 @@
 package shapeMensuration;
 
 public class Quadrilateral {
+    public Quadrilateral(double base1, double base2) {
+        this.base1 = base1;
+        this.base2 = base2;
+    }
+
+    private double base1;
+    private double base2;
+
 }
