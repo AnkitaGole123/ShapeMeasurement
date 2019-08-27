766629826dc29484425fd7731bb18fb21a270fc7
Ankitagole <ankita17@navgurukul.org>
2019-08-27 22:26:09 +0530

    override getVolume method for cylinder

--- src/main/java/All3DShapes/Cylinder.java
+++ src/main/java/All3DShapes/Cylinder.java
@@ -1,5 +1,21 @@
 package All3DShapes;
 
-public class Cylinder {
+public class Cylinder implements Shapes3D{
+    public Cylinder(double radius, double height) {
+        this.radius = radius;
+        this.height = height;
+    }
 
+    double radius;
+    double height;
+
+    @Override
+    public double getVolume() {
+        return (22*radius*radius*height)/7;
+    }
+
+    @Override
+    public double getSurfaceArea() {
+        return 0;
+    }
 }
