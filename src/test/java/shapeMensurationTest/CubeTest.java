2e8ab7195d65581f96460787ba7758b659cd060f
Ankitagole <ankita17@navgurukul.org>
2019-08-27 22:42:20 +0530

    testcase for getSurfaceArea method for cube

--- src/test/java/shapeMensurationTest/CubeTest.java
+++ src/test/java/shapeMensurationTest/CubeTest.java
@@ -8,10 +8,18 @@ import static org.junit.Assert.assertEquals;
 
 public class CubeTest {
     @Test
-    public void itShouldGiveTheVolumeOfCylinder() {
+    public void itShouldGiveTheVolumeOfCube() {
         Cube cube = new Cube(3);
         double expected = 54;
         double delta = 1;
         assertEquals(expected,cube.getVolume(),delta);
+    }
+
+    @Test
+    public void itShouldGiveTheSurfaceAreaOfCube() {
+        Cube cube = new Cube(3);
+        double expected = 27;
+        double delta = 1;
+        assertEquals(expected,cube.getSurfaceArea(),delta);
     }
 }
