2feaa9cd4bab90575280b45ba2f31412072d292f
Ankitagole <ankita17@navgurukul.org>
2019-08-28 09:58:14 +0530

    refactor. testcase of cube

--- src/test/java/shapeMensurationTest/CubeTest.java
+++ src/test/java/shapeMensurationTest/CubeTest.java
@@ -10,7 +10,7 @@ public class CubeTest {
     @Test
     public void itShouldGiveTheVolumeOfCube() {
         Cube cube = new Cube(3);
-        double expected = 54;
+        double expected = 27;
         double delta = 1;
         assertEquals(expected,cube.getVolume(),delta);
     }
@@ -18,7 +18,7 @@ public class CubeTest {
     @Test
     public void itShouldGiveTheSurfaceAreaOfCube() {
         Cube cube = new Cube(3);
-        double expected = 27;
+        double expected = 54;
         double delta = 1;
         assertEquals(expected,cube.getSurfaceArea(),delta);
     }
